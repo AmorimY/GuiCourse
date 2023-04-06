d3af72c1956df04d127ecfaa50be7f035037948e
AmorimY <rafael.amorim.praia@gmail.com>
2023-04-05 22:56:30 -0300

    Frame

---
+++ src/Labels/MyFrame.java
@@ -0,0 +1,19 @@
+package Labels;
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+	MyFrame(){
+		this.setTitle("Jthis title goes here");//set the title of the frame 
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//exit out out the application by cliking on the X
+		this.setResizable(false);//prevent frame from being resized 
+		this.setSize(420, 420); // sets the dimension of the frame 
+		this.setVisible(true);// Makes visible 
+		ImageIcon image = new ImageIcon("logo.png");//create a image icon
+		this.setIconImage(image.getImage());//change the icon of the frame
+		this.getContentPane().setBackground(new Color(34,77,31)); //Change color of background 
+	}
+
+}
