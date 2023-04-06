347f4356b24927ea99d69b45d3217ddce2190250
Your Name <rafael.amorim.praia@gmail.com>
2023-04-06 17:14:10 -0300

    Labels

--- src/Labels/Main.java
+++ src/Labels/Main.java
@@ -1,25 +1,49 @@
 package Labels;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.Border;
+
 public class Main {
 
 	public static void main(String[] args) {
-		//JFrame = a GUI window to add components to 
-		/*
+		//Jlabel = a GUI display area for a string of text,an image or both
+		
+		ImageIcon image = new ImageIcon("dude.jpg");
+		Border border = BorderFactory.createLineBorder(Color.green,3);
+		
+		JLabel label = new JLabel(); // creat a label 
+		label.setText("hello world"); // set text of label 
+		label.setIcon(image);
+		label.setHorizontalTextPosition(JLabel.CENTER);//set text LEFT,CENTER,RIGHT of imageicon
+		label.setVerticalTextPosition(JLabel.TOP);//Set TOP CENTER BOTTOM of imageicon
+		label.setForeground(new Color(128,193,5));//set the color of font
+		label.setFont(new Font("MV Boli",Font.PLAIN,20));//set font of text
+		label.setIconTextGap(-25); // set the gap of text to image 
+		label.setBackground(Color.BLACK);// set background color 
+		label.setOpaque(true);//display background color 
+		label.setBorder(border);
+		label.setVerticalAlignment(JLabel.CENTER); // set vertical position of icon+text within label
+		label.setHorizontalAlignment(JLabel.CENTER);//set horizontal position of icon+text whitin label
+		//label.setBounds(10,10,360,360);//set X,Y position within frame as wll as dimensions
+		
+		
+		
 		JFrame frame = new JFrame(); //creates a frame
-		frame.setTitle("JFrame title goes here");//set the title of the frame 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//exit out out the application by cliking on the X
-		frame.setResizable(false);//prevent frame from being resized 
-		frame.setSize(420, 420); // sets the dimension of the frame 
+		frame.setSize(500, 500); // sets the dimension of the frame 
+		//frame.setLayout(null);
 		frame.setVisible(true);// Makes visible 
+		frame.add(label);
+		frame.pack();//the last thing,will break the app if puts before adding all components 
 		
-		ImageIcon image = new ImageIcon("logo.png");//create a image icon
-		
-		frame.setIconImage(image.getImage());//change the icon of the frame
-		
-		frame.getContentPane().setBackground(new Color(34,77,31)); //Change color of background 
 
-		 */
-		MyFrame myFrame = new MyFrame();
+		 
  }
 
 }
