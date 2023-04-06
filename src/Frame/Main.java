d15416c96cc183086e90fcd68326679c9b2e70fe
Your Name <rafael.amorim.praia@gmail.com>
2023-04-06 15:08:14 -0300

    Jlabel

--- src/Frame/Main.java
+++ src/Frame/Main.java
@@ -7,22 +7,7 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args) {
-		//JFrame = a GUI window to add components to 
-		/*
-		JFrame frame = new JFrame(); //creates a frame
-		frame.setTitle("JFrame title goes here");//set the title of the frame 
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//exit out out the application by cliking on the X
-		frame.setResizable(false);//prevent frame from being resized 
-		frame.setSize(420, 420); // sets the dimension of the frame 
-		frame.setVisible(true);// Makes visible 
-		
-		ImageIcon image = new ImageIcon("logo.png");//create a image icon
-		
-		frame.setIconImage(image.getImage());//change the icon of the frame
-		
-		frame.getContentPane().setBackground(new Color(34,77,31)); //Change color of background 
 
-		 */
 		MyFrame myFrame = new MyFrame();
  }
 
